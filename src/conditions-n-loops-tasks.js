06736217d92ff003a12973552e22c11a997e3c25
Maslovars <maslovars@gmail.com>
2024-01-20 03:35:31 +0300

    feat: add spiral matrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,10 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) return a;
+  if (b > c) return b;
+  return c;
 }
 
 /**
@@ -60,8 +62,11 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) return true;
+  if (queen.y === king.y) return true;
+  if (Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)) return true;
+  return false;
 }
 
 /**
@@ -82,8 +87,11 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === 0 || b === 0 || c === 0) return false;
+  if (a + b < c || a + c < b || b + c < a) return false;
+  if (a === b || a === c || b === c) return true;
+  return false;
 }
 
 /**
@@ -100,8 +108,21 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  if (num >= 10) return `X${convertToRomanNumerals(num - 10)}`;
+  if (num >= 9) {
+    return `IX${convertToRomanNumerals(num - 9)}`;
+  }
+  if (num >= 5) {
+    return `V${convertToRomanNumerals(num - 5)}`;
+  }
+  if (num >= 4) {
+    return `IV${convertToRomanNumerals(num - 4)}`;
+  }
+  if (num >= 1) {
+    return `I${convertToRomanNumerals(num - 1)}`;
+  }
+  return '';
 }
 
 /**
@@ -119,8 +140,69 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let res = '';
+
+  for (let i = 0; i < numberStr.length; i += 1) {
+    switch (numberStr[i]) {
+      case '-':
+        res += 'minus ';
+        break;
+
+      case '0':
+        res += 'zero ';
+        break;
+
+      case '1':
+        res += 'one ';
+        break;
+
+      case '2':
+        res += 'two ';
+        break;
+
+      case '3':
+        res += 'three ';
+        break;
+
+      case '4':
+        res += 'four ';
+        break;
+
+      case '5':
+        res += 'five ';
+        break;
+
+      case '6':
+        res += 'six ';
+        break;
+
+      case '7':
+        res += 'seven ';
+        break;
+
+      case '8':
+        res += 'eight ';
+        break;
+
+      case '9':
+        res += 'nine ';
+        break;
+
+      case '.':
+      case ',':
+        res += 'point ';
+        break;
+
+      default:
+        break;
+    }
+  }
+  let withoutLastSpace = '';
+  for (let i = 0; i < res.length - 1; i += 1) {
+    withoutLastSpace += res[i];
+  }
+  return withoutLastSpace;
 }
 
 /**
@@ -135,8 +217,12 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let res = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    res += str[i];
+  }
+  return res === str;
 }
 
 /**
@@ -153,8 +239,12 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let res = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) res = i;
+  }
+  return res;
 }
 
 /**
@@ -172,8 +262,10 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  if (num < 10) return num === digit;
+  if (num % 10 === digit) return true;
+  return isContainNumber(Math.floor(num / 10), digit);
 }
 
 /**
@@ -189,8 +281,21 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 1; i < arr.length; i += 1) {
+    let leftSum = 0;
+    let rightSum = 0;
+    for (let j = 0; j < i; j += 1) {
+      leftSum += arr[j];
+    }
+    for (let x = i + 1; x < arr.length; x += 1) {
+      rightSum += arr[x];
+    }
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -214,8 +319,55 @@ function getBalanceIndex(/* arr */) {
  *          [10, 9,  8,  7]
  *        ]
  */
-function getSpiralMatrix(/* size */) {
-  throw new Error('Not implemented');
+function getSpiralMatrix(size) {
+  let top = 0;
+  let left = 0;
+  let right = size - 1;
+  let bottom = size - 1;
+  let nextNumber = 1;
+  const spiralMatrix = [];
+  for (let i = 0; i < size; i += 1) {
+    spiralMatrix[i] = [];
+    for (let j = 0; j < size; j += 1) {
+      spiralMatrix[i][j] = [];
+    }
+  }
+
+  while (top <= bottom && left <= right) {
+    let i = top;
+
+    while (i <= right) {
+      spiralMatrix[top][i] = nextNumber;
+      i += 1;
+      nextNumber += 1;
+    }
+    top += 1;
+    i = top;
+
+    while (i <= bottom) {
+      spiralMatrix[i][right] = nextNumber;
+      i += 1;
+      nextNumber += 1;
+    }
+    right -= 1;
+    i = right;
+
+    while (i >= left) {
+      spiralMatrix[bottom][i] = nextNumber;
+      i -= 1;
+      nextNumber += 1;
+    }
+    bottom -= 1;
+    i = bottom;
+
+    while (i >= top) {
+      spiralMatrix[i][left] = nextNumber;
+      i -= 1;
+      nextNumber += 1;
+    }
+    left += 1;
+  }
+  return spiralMatrix;
 }
 
 /**
